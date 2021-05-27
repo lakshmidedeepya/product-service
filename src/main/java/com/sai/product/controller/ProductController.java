b4fbb393c666c28c2435a78e1f9d0c43c1d6e221
unknown <w2cluster841@iiht.tech>
2021-05-27 11:32:25 +0530

    added config changes

--- src/main/java/com/sai/product/controller/ProductController.java
+++ src/main/java/com/sai/product/controller/ProductController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +19,19 @@ import com.sai.product.model.Product;
 import com.sai.product.service.ProductService;
 
 @RestController
+@RefreshScope
 @RequestMapping("/product")
 public class ProductController {
+	@Value("${message:default}")
+	String message;
+	
 	@Autowired
 	private ProductService prodser;
+	@GetMapping("/test")
+	public String getProduct(@RequestParam("name") String name) {
+		return "Hello "+name+" "+this.message;
+		
+	}
 @PostMapping
 public Product createProduct(@RequestBody Product req) {
 	return prodser.create(req);
